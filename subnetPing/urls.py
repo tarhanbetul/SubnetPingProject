f4bf709d37dddc3b6393202644f2caf4aae61490
tarhanbetul <betul.tarhan18@gmail.com>
2023-10-04 14:10:24 +0300

    django bugfix

--- subnetPing/urls.py
+++ subnetPing/urls.py
@@ -2,6 +2,5 @@ from django.urls import path
 from . import views  
 
 urlpatterns = [
-    path('get_subnet_ips/', views.get_subnet_ips, name='get_subnet_ips'),
-    path('hello-world/', views.hello_world, name='hello-world'),
+    path('get_subnet_ips/', views.get_subnet_ips, name='get_subnet_ips')
 ]
